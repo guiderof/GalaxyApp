4af45cf801c0dfd74c3836aaa0883d40842a0260
ChanonRd <chanon@Sevenswords.local>
2016-05-15 16:00:46 +0700

    Developed product list page

---
+++ product_list.php
@@ -0,0 +1,29 @@
+
+<!DOCTYPE html>
+<html>
+<head>
+    <title>ReadyGalaxy</title>
+    <meta charset="UTF-8"><meta charset="UTF-8">
+    <!-- jQuery -->
+    <script src="js/jquery-1.12.3.min.js"></script>
+
+    <!-- Bootstrap -->
+    <link href="css/bootstrap.min.css" rel="stylesheet">
+    <script src="js/bootstrap.min.js"></script>
+
+    <script type="text/javascript" src="js/product_list.js"></script>
+
+
+    <link href="css/template.css" rel="stylesheet">
+</head>
+<body>
+    <header>
+        <h4>ReadyGalaxy</h4>
+    </header>
+    <div class="container-fluid">
+        <div id="product_list_container" class="row">
+            <!-- Product list here -->
+        </div>
+    </div>
+</body>
+</html>
